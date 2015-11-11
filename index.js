cf29f4d447fe1538d7f4f21a5a50e3f7633cba49
Mary Dickson Diaz <mdickson@gmail.com>
2015-11-10 18:26:50 -0800

    remove port for deploy

--- index.js
+++ index.js
@@ -124,9 +124,9 @@ app.get('/', function (req, res) {
   res.send(renderPoem());
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });

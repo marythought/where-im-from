250284eb01bfdb82fc15eaabcb69e1c653f1748b
Mary Dickson Diaz <mdickson@gmail.com>
2015-11-17 00:37:08 -0800

    refactor where i'm from

--- index.js
+++ index.js
@@ -1,119 +1,54 @@
 var express = require('express');
 var app = express();
-
 var rita = require('rita');
 var lexicon = new rita.RiLexicon();
-var brands = require('./lib/brands').Brands;
-var interjections = require('./lib/interjections').Interjections;
-var colors = require('./lib/colors').Colors;
-var rooms = require('./lib/rooms').Rooms;
-var senseWords = ["tasted", "smelled", "sounded", "felt", "looked"]
-var foods = require('./lib/foods').Foods;
-var animals = require('./lib/animals').Animals;
-var trees = require('./lib/trees');
-var names = require('./lib/names').Names;
-var cliques = require('./lib/cliques').Cliques;
-var quotes = require('./lib/shortQuotes').Quotes;
-var writings = require('./lib/writingForms').WritingForms;
-var cities = require('./lib/cities').Cities;
-var cookingWords = require('./lib/cookingWords').CookingWords;
-var drinkAdjectives = require('./lib/drinkAdjectives').DrinkAdjectives;
-var drinks = require('./lib/drinks').Drinks;
-var bodyParts = require('./lib/body').BodyParts;
-var relatives = require('./lib/relatives').Relatives;
-var hurts = require('./lib/waysToGetHurt').Hurts;
-var furniture = require('./lib/furniture').Furniture;
-var senseNouns = ["sight", "hearing", "touch", "smell", "taste"];
-var prepositions = require('./lib/prepositions').Prepositions
+var words = require('./lib/words').wordTypes;
+
 var poem
 
 String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+function sample(array){
+  return array[Math.floor(Math.random()*array.length)];
+};
+
+// poem += "from " + sample(word.brands) +  " and " + …
+
 function renderPoem(){
-  var number = lexicon.randomWord('cd')
-  var pluralNoun = lexicon.randomWord('nns', 2)
-  var sixSylNoun = lexicon.randomWord('nn', 6)
-  var noun2 = lexicon.randomWord('nns', 3)
-  var noun3 = lexicon.randomWord('nns', 1)
-  var noun4 = lexicon.randomWord('nn', 1)
-  var ingNoun = lexicon.randomWord('vbg', 2)
-  var pluralNoun2 = lexicon.randomWord('nns', 2)
-  var adjective = lexicon.randomWord('jj', 3)
-  var adjective2 = lexicon.randomWord('jj', 3)
-  var adjective3 = lexicon.randomWord('jj', 2)
-  var adjective4 = lexicon.randomWord('jj', 1)
-  var adjective5 = lexicon.randomWord('jj', 1)
-  var edVerb1 = lexicon.randomWord('vbd', 2)
-  var edVerb2 = lexicon.randomWord('vbd', 2)
-  var edVerb3 = lexicon.randomWord('vbd', 1)
-  var edVerb4 = lexicon.randomWord('vbd', 1)
-  var animal = animals[Math.floor(Math.random()*animals.length)]
-  var bodyPart1 = bodyParts[Math.floor(Math.random()*bodyParts.length)]
-  var bodyPart2 = bodyParts[Math.floor(Math.random()*bodyParts.length)]
-  var brand = brands[Math.floor(Math.random()*brands.length)]
-  var city1 = cities[Math.floor(Math.random()*cities.length)]
-  var city2 = cities[Math.floor(Math.random()*cities.length)]
-  var clique1 = cliques[Math.floor(Math.random()*cliques.length)]
-  var clique2 = cliques[Math.floor(Math.random()*cliques.length)]
-  var color = colors[Math.floor(Math.random()*colors.length)]
-  var cookingWord = cookingWords[Math.floor(Math.random()*cookingWords.length)]
-  var drink = drinks[Math.floor(Math.random()*drinks.length)]
-  var drinkAdjective = drinkAdjectives[Math.floor(Math.random()*drinkAdjectives.length)]
-  var food = foods[Math.floor(Math.random()*foods.length)]
-  var food2 = foods[Math.floor(Math.random()*foods.length)]
-  var food3 = foods[Math.floor(Math.random()*foods.length)]
-  var furniture1 = furniture[Math.floor(Math.random()*furniture.length)]
-  var interjection1 = interjections[Math.floor(Math.random()*interjections.length)]
-  var interjection2 = interjections[Math.floor(Math.random()*interjections.length)]
-  var name1 = names[Math.floor(Math.random()*names.length)]
-  var name2 = names[Math.floor(Math.random()*names.length)]
-  var preposition = prepositions[Math.floor(Math.random()*prepositions.length)]
-  var preposition2 = prepositions[Math.floor(Math.random()*prepositions.length)]
-  var quote = quotes[Math.floor(Math.random()*quotes.length)]
-  var quote2 = quotes[Math.floor(Math.random()*quotes.length)]
-  var relative1 = relatives[Math.floor(Math.random()*relatives.length)]
-  var relative2 = relatives[Math.floor(Math.random()*relatives.length)]
-  var room = rooms[Math.floor(Math.random()*rooms.length)]
-  var senseNoun = senseNouns[Math.floor(Math.random()*senseWords.length)]
-  var senseWord = senseWords[Math.floor(Math.random()*senseWords.length)]
-  var tree1 = trees.First[Math.floor(Math.random()*trees.First.length)] + " " + trees.Second[Math.floor(Math.random()*trees.Second.length)]
-  var tree2 = trees.First[Math.floor(Math.random()*trees.First.length)] + " " + trees.First[Math.floor(Math.random()*trees.First.length)] + " " + trees.Second[Math.floor(Math.random()*trees.Second.length)]
-  var wayToGetHurt = hurts[Math.floor(Math.random()*hurts.length)]
-  var writing = writings[Math.floor(Math.random()*writings.length)]
   poem = "<h1>Where I'm From</h1>";
-  poem += "<p>I am from " + pluralNoun + ",<br>";
-  poem += "from " + brand.capitalize() + " and " + sixSylNoun + ".<br>";
-  poem += "I am from the " + noun4 + " under the " + room + ".<br>";
-  poem += "(" + color.capitalize() + ", " + adjective + ",<br>";
-  poem += "it " + senseWord + " like " + food + ".)<br>";
-  poem += "I am from the " + tree1 + "<br>";
-  poem += "the " + tree2 + "<br>";
-  poem += "whose " + adjective3 + " " + noun3 + " I remember<br>";
+  poem += "<p>I am from " + lexicon.randomWord('nns', 2) + ",<br>";
+  poem += "from " + sample(words.brands).capitalize() + " and " + lexicon.randomWord('nn', 6) + ".<br>";
+  poem += "I am from the " + lexicon.randomWord('nn', 1) + " under the " + sample(words.rooms) + ".<br>";
+  poem += "(" + sample(words.colors).capitalize() + ", " + lexicon.randomWord('jj', 3) + ",<br>";
+  poem += "it " + sample(words.senseWords) + " like " + sample(words.foods) + ".)<br>";
+  poem += "I am from the " + sample(words.treeAdjectives) + " " + sample(words.trees) + "<br>";
+  poem += "the " + sample(words.treeAdjectives) + " " + sample(words.treeAdjectives) + " " + sample(words.trees) + "<br>";
+  poem += "whose " + lexicon.randomWord('jj', 2) + " " + lexicon.randomWord('nns', 1) + " I remember<br>";
   poem += "as if they were my own.<br>";
   poem += "<br>";
-  poem += "I'm from " + food2 + " and " + noun2 + ",<br>";
-  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;from " + name1 + " and " + name2 +".<br>";
-  poem += "I'm from the " + clique1 +"<br>";
-  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and the " + clique2 + ",<br>";
-  poem += "from '" + interjection1 + "' and '" + interjection2 + "'!<br>";
-  poem += "I'm from '" + quote + "'<br>";
-  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'" + quote2 + "'<br>";
-  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and " + number + " " + writing + " I can say myself.<br>";
+  poem += "I'm from " + sample(words.foods) + " and " + lexicon.randomWord('nns', 3) + ",<br>";
+  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;from " + sample(words.names) + " and " + sample(words.names) +".<br>";
+  poem += "I'm from the " + sample(words.cliques) +"<br>";
+  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and the " + sample(words.cliques) + ",<br>";
+  poem += "from '" + sample(words.interjections) + "' and '" + sample(words.interjections) + "'!<br>";
+  poem += "I'm from '" + sample(words.quotes) + "'<br>";
+  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'" + sample(words.quotes) + "'<br>";
+  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and " + lexicon.randomWord('cd') + " " + sample(words.writings) + " I can say myself.<br>";
   poem += "<br>";
-  poem += "I'm from " + city1 + " and " + city2 + ",<br>";
-  poem += cookingWord + " " + food3 + " and " + drinkAdjective + " " + drink + ".<br>";
-  poem += "From the " + bodyPart1 + " my " + relative1 + " " + edVerb4 + "<br>";
-  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + wayToGetHurt + ",<br>";
-  poem += "the " + bodyPart2 + " my " + relative2 + " " + edVerb3 + " to keep their " + senseNoun + ".<br>";
+  poem += "I'm from " + sample(words.cities) + " and " + sample(words.cities) + ",<br>";
+  poem += sample(words.cookingWords) + " " + sample(words.foods) + " and " + sample(words.drinkAdjectives) + " " + sample(words.drinks) + ".<br>";
+  poem += "From the " + sample(words.bodyParts) + " my " + sample(words.relatives) + " " + lexicon.randomWord('vbd', 1) + "<br>";
+  poem += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sample(words.hurts) + ",<br>";
+  poem += "the " + sample(words.animals) + " my " + sample(words.relatives) + " " + lexicon.randomWord('vbd', 1) + " to keep their " + sample(words.senseNouns) + ".<br>";
   poem += "<br>";
-  poem += preposition.capitalize() + " my " + furniture1 + " was a " + adjective5 + " box<br>";
-  poem += "holding " + adjective4 + " " + pluralNoun2 + ",<br>";
+  poem += sample(words.prepositions).capitalize() + " my " + sample(words.furniture) + " was a " + lexicon.randomWord('jj', 1) + " box<br>";
+  poem += "holding " + lexicon.randomWord('jj', 1) + " " + lexicon.randomWord('nns', 2) + ",<br>";
   poem += "a sift of lost faces<br>";
-  poem += "to drift " + preposition2 + " my dreams.<br>";
+  poem += "to drift " + sample(words.prepositions) + " my dreams.<br>";
   poem += "I am from those moments--<br>";
-  poem += edVerb1 + " before I " + edVerb2 + "--<br>";
+  poem += lexicon.randomWord('vbd', 2) + " before I " + lexicon.randomWord('vbd', 2) + "--<br>";
   poem += "leaf-fall from the family tree.<br></p>";
   poem += "<br><p> poem generator by <a href='http://www.marydickson.com'>Mary Dickson</a> based on the original";
   poem += " by <a href='http://www.georgeellalyon.com/where.html'>George Ella Lyon</a>."
